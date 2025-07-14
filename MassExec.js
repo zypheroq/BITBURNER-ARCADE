6def8268cbc9e99f2ac07764d8a970e79de2ff1e
zypheroq <158800359+zypheroq@users.noreply.github.com>
2025-07-14 02:18:41 -0700

    Create MassExec.js
    added MassExec

---
+++ MassExec.js
@@ -0,0 +1,239 @@
+// === SCP & Start hacking script on each nuked server ===
+
+// 0‑port servers
+scp("hacking.script", "home", "foodnstuff");
+exec("hacking.script", "foodnstuff", Math.max(1, Math.floor(getServerRam("foodnstuff")[0] / 2.6)));
+scp("hacking.script", "home", "sigma-cosmetics");
+exec("hacking.script", "sigma-cosmetics", Math.max(1, Math.floor(getServerRam("sigma-cosmetics")[0] / 2.6)));
+scp("hacking.script", "home", "nectar-net");
+exec("hacking.script", "nectar-net", Math.max(1, Math.floor(getServerRam("nectar-net")[0] / 2.6)));
+scp("hacking.script", "home", "joesguns");
+exec("hacking.script", "joesguns", Math.max(1, Math.floor(getServerRam("joesguns")[0] / 2.6)));
+scp("hacking.script", "home", "hong-fang-tea");
+exec("hacking.script", "hong-fang-tea", Math.max(1, Math.floor(getServerRam("hong-fang-tea")[0] / 2.6)));
+scp("hacking.script", "home", "harakiri-sushi");
+exec("hacking.script", "harakiri-sushi", Math.max(1, Math.floor(getServerRam("harakiri-sushi")[0] / 2.6)));
+
+// 1‑port servers
+scp("hacking.script", "home", "zer0");
+exec("hacking.script", "zer0", Math.max(1, Math.floor(getServerRam("zer0")[0] / 2.6)));
+scp("hacking.script", "home", "CSEC");
+exec("hacking.script", "CSEC", Math.max(1, Math.floor(getServerRam("CSEC")[0] / 2.6)));
+scp("hacking.script", "home", "neo-net");
+exec("hacking.script", "neo-net", Math.max(1, Math.floor(getServerRam("neo-net")[0] / 2.6)));
+scp("hacking.script", "home", "max-hardware");
+exec("hacking.script", "max-hardware", Math.max(1, Math.floor(getServerRam("max-hardware")[0] / 2.6)));
+scp("hacking.script", "home", "iron-gym");
+exec("hacking.script", "iron-gym", Math.max(1, Math.floor(getServerRam("iron-gym")[0] / 2.6)));
+
+// 2‑port servers
+scp("hacking.script", "home", "phantasy");
+exec("hacking.script", "phantasy", Math.max(1, Math.floor(getServerRam("phantasy")[0] / 2.6)));
+scp("hacking.script", "home", "crush-fitness");
+exec("hacking.script", "crush-fitness", Math.max(1, Math.floor(getServerRam("crush-fitness")[0] / 2.6)));
+scp("hacking.script", "home", "silver-helix");
+exec("hacking.script", "silver-helix", Math.max(1, Math.floor(getServerRam("silver-helix")[0] / 2.6)));
+scp("hacking.script", "home", "the-hub");
+exec("hacking.script", "the-hub", Math.max(1, Math.floor(getServerRam("the-hub")[0] / 2.6)));
+scp("hacking.script", "home", "johnson-ortho");
+exec("hacking.script", "johnson-ortho", Math.max(1, Math.floor(getServerRam("johnson-ortho")[0] / 2.6)));
+scp("hacking.script", "home", "avmnite-02h");
+exec("hacking.script", "avmnite-02h", Math.max(1, Math.floor(getServerRam("avmnite-02h")[0] / 2.6)));
+scp("hacking.script", "home", "omega-net");
+exec("hacking.script", "omega-net", Math.max(1, Math.floor(getServerRam("omega-net")[0] / 2.6)));
+
+// 3‑port servers
+scp("hacking.script", "home", "comptek");
+exec("hacking.script", "comptek", Math.max(1, Math.floor(getServerRam("comptek")[0] / 2.6)));
+scp("hacking.script", "home", "summit-uni");
+exec("hacking.script", "summit-uni", Math.max(1, Math.floor(getServerRam("summit-uni")[0] / 2.6)));
+scp("hacking.script", "home", "rho-construction");
+exec("hacking.script", "rho-construction", Math.max(1, Math.floor(getServerRam("rho-construction")[0] / 2.6)));
+scp("hacking.script", "home", "millenium-fitness");
+exec("hacking.script", "millenium-fitness", Math.max(1, Math.floor(getServerRam("millenium-fitness")[0] / 2.6)));
+scp("hacking.script", "home", "catalyst");
+exec("hacking.script", "catalyst", Math.max(1, Math.floor(getServerRam("catalyst")[0] / 2.6)));
+scp("hacking.script", "home", "rothman-uni");
+exec("hacking.script", "rothman-uni", Math.max(1, Math.floor(getServerRam("rothman-uni")[0] / 2.6)));
+scp("hacking.script", "home", "netlink");
+exec("hacking.script", "netlink", Math.max(1, Math.floor(getServerRam("netlink")[0] / 2.6)));
+scp("hacking.script", "home", "I.I.I.I");
+exec("hacking.script", "I.I.I.I", Math.max(1, Math.floor(getServerRam("I.I.I.I")[0] / 2.6)));
+
+// 4‑port servers
+scp("hacking.script", "home", "lexo-corp");
+exec("hacking.script", "lexo-corp", Math.max(1, Math.floor(getServerRam("lexo-corp")[0] / 2.6)));
+scp("hacking.script", "home", "global-pharm");
+exec("hacking.script", "global-pharm", Math.max(1, Math.floor(getServerRam("global-pharm")[0] / 2.6)));
+scp("hacking.script", "home", "syscore");
+exec("hacking.script", "syscore", Math.max(1, Math.floor(getServerRam("syscore")[0] / 2.6)));
+scp("hacking.script", "home", "alpha-ent");
+exec("hacking.script", "alpha-ent", Math.max(1, Math.floor(getServerRam("alpha-ent")[0] / 2.6)));
+scp("hacking.script", "home", "unitalife");
+exec("hacking.script", "unitalife", Math.max(1, Math.floor(getServerRam("unitalife")[0] / 2.6)));
+scp("hacking.script", "home", "zb-def");
+exec("hacking.script", "zb-def", Math.max(1, Math.floor(getServerRam("zb-def")[0] / 2.6)));
+scp("hacking.script", "home", "univ-energy");
+exec("hacking.script", "univ-energy", Math.max(1, Math.floor(getServerRam("univ-energy")[0] / 2.6)));
+scp("hacking.script", "home", "snap-fitness");
+exec("hacking.script", "snap-fitness", Math.max(1, Math.floor(getServerRam("snap-fitness")[0] / 2.6)));
+scp("hacking.script", "home", "nova-med");
+exec("hacking.script", "nova-med", Math.max(1, Math.floor(getServerRam("nova-med")[0] / 2.6)));
+scp("hacking.script", "home", "run4theh111z");
+exec("hacking.script", "run4theh111z", Math.max(1, Math.floor(getServerRam("run4theh111z")[0] / 2.6)));
+scp("hacking.script", "home", "applied-energetics");
+exec("hacking.script", "applied-energetics", Math.max(1, Math.floor(getServerRam("applied-energetics")[0] / 2.6)));
+scp("hacking.script", "home", "aevum-police");
+exec("hacking.script", "aevum-police", Math.max(1, Math.floor(getServerRam("aevum-police")[0] / 2.6)));
+scp("hacking.script", "home", ".");
+exec("hacking.script", ".", Math.max(1, Math.floor(getServerRam(".")[0] / 2.6)));
+
+// 5‑port servers
+scp("hacking.script", "home", "zb-institute");
+exec("hacking.script", "zb-institute", Math.max(1, Math.floor(getServerRam("zb-institute")[0] / 2.6)));
+scp("hacking.script", "home", "galactic-cyber");
+exec("hacking.script", "galactic-cyber", Math.max(1, Math.floor(getServerRam("galactic-cyber")[0] / 2.6)));
+scp("hacking.script", "home", "aerocorp");
+exec("hacking.script", "aerocorp", Math.max(1, Math.floor(getServerRam("aerocorp")[0] / 2.6)));
+scp("hacking.script", "home", "defcomm");
+exec("hacking.script", "defcomm", Math.max(1, Math.floor(getServerRam("defcomm")[0] / 2.6)));
+scp("hacking.script", "home", "omnia");
+exec("hacking.script", "omnia", Math.max(1, Math.floor(getServerRam("omnia")[0] / 2.6)));
+scp("hacking.script", "home", "solaris");
+exec("hacking.script", "solaris", Math.max(1, Math.floor(getServerRam("solaris")[0] / 2.6)));
+scp("hacking.script", "home", "zeus-med");
+exec("hacking.script", "zeus-med", Math.max(1, Math.floor(getServerRam("zeus-med")[0] / 2.6)));
+scp("hacking.script", "home", "taiyang-digital");
+exec("hacking.script", "taiyang-digital", Math.max(1, Math.floor(getServerRam("taiyang-digital")[0] / 2.6)));
+scp("hacking.script", "home", "microdyne");
+exec("hacking.script", "microdyne", Math.max(1, Math.floor(getServerRam("microdyne")[0] / 2.6)));
+scp("hacking.script", "home", "vitalife");
+exec("hacking.script", "vitalife", Math.max(1, Math.floor(getServerRam("vitalife")[0] / 2.6)));
+scp("hacking.script", "home", "omnitek");
+exec("hacking.script", "omnitek", Math.max(1, Math.floor(getServerRam("omnitek")[0] / 2.6)));
+scp("hacking.script", "home", "b-and-a");
+exec("hacking.script", "b-and-a", Math.max(1, Math.floor(getServerRam("b-and-a")[0] / 2.6)));
+scp("hacking.script", "home", "The-Cave");
+exec("hacking.script", "The-Cave", Math.max(1, Math.floor(getServerRam("The-Cave")[0] / 2.6)));
+scp("hacking.script", "home", "w0r1d_d43m0n");
+exec("hacking.script", "w0r1d_d43m0n", Math.max(1, Math.floor(getServerRam("w0r1d_d43m0n")[0] / 2.6)));
+scp("hacking.script", "home", "blade");
+exec("hacking.script", "blade", Math.max(1, Math.floor(getServerRam("blade")[0] / 2.6)));
+scp("hacking.script", "home", "megacorp");
+exec("hacking.script", "megacorp", Math.max(1, Math.floor(getServerRam("megacorp")[0] / 2.6)));
+scp("hacking.script", "home", "fulcrumassets");
+exec("hacking.script", "fulcrumassets", Math.max(1, Math.floor(getServerRam("fulcrumassets")[0] / 2.6)));
+scp("hacking.script", "home", "powerhouse-fitness");
+exec("hacking.script", "powerhouse-fitness", Math.max(1, Math.floor(getServerRam("powerhouse-fitness")[0] / 2.6)));
+scp("hacking.script", "home", "titan-labs");
+exec("hacking.script", "titan-labs", Math.max(1, Math.floor(getServerRam("titan-labs")[0] / 2.6)));
+scp("hacking.script", "home", "helios");
+exec("hacking.script", "helios", Math.max(1, Math.floor(getServerRam("helios")[0] / 2.6)));
+scp("hacking.script", "home", "deltaone");
+exec("hacking.script", "deltaone", Math.max(1, Math.floor(getServerRam("deltaone")[0] / 2.6)));
+scp("hacking.script", "home", "icarus");
+exec("hacking.script", "icarus", Math.max(1, Math.floor(getServerRam("icarus")[0] / 2.6)));
+scp("hacking.script", "home", "infocomm");
+exec("hacking.script", "infocomm", Math.max(1, Math.floor(getServerRam("infocomm")[0] / 2.6)));
+scp("hacking.script", "home", "fulcrumtech");
+exec("hacking.script", "fulcrumtech", Math.max(1, Math.floor(getServerRam("fulcrumtech")[0] / 2.6)));
+scp("hacking.script", "home", "kuai-gong");
+exec("hacking.script", "kuai-gong", Math.max(1, Math.floor(getServerRam("kuai-gong")[0] / 2.6)));
+scp("hacking.script", "home", "stormtech");
+exec("hacking.script", "stormtech", Math.max(1, Math.floor(getServerRam("stormtech")[0] / 2.6)));
+scp("hacking.script", "home", "4sigma");
+exec("hacking.script", "4sigma", Math.max(1, Math.floor(getServerRam("4sigma")[0] / 2.6)));
+scp("hacking.script", "home", "nwo");
+exec("hacking.script", "nwo", Math.max(1, Math.floor(getServerRam("nwo")[0] / 2.6)));
+scp("hacking.script", "home", "ecorp");
+exec("hacking.script", "ecorp", Math.max(1, Math.floor(getServerRam("ecorp")[0] / 2.6)));
+scp("hacking.script", "home", "darkweb");
+exec("hacking.script", "darkweb", Math.max(1, Math.floor(getServerRam("darkweb")[0] / 2.6)));
+
+tprint("Spreaded and Executed! Continuing on Purchased Servers.");
+
+if(true){
+scp("hacking.script", "home", "Server");
+exec("hacking.script", "Server", Math.max(1, Math.floor(getServerRam("Server")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0");
+exec("hacking.script", "Server-0", Math.max(1, Math.floor(getServerRam("Server-0")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1");
+exec("hacking.script", "Server-0-1", Math.max(1, Math.floor(getServerRam("Server-0-1")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2");
+exec("hacking.script", "Server-0-1-2", Math.max(1, Math.floor(getServerRam("Server-0-1-2")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3");
+exec("hacking.script", "Server-0-1-2-3", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4");
+exec("hacking.script", "Server-0-1-2-3-4", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5");
+exec("hacking.script", "Server-0-1-2-3-4-5", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6");
+exec("hacking.script", "Server-0-1-2-3-4-5-6", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24")[0] / 2.6)));
+
+scp("hacking.script", "home", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24-25");
+exec("hacking.script", "Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24-25", Math.max(1, Math.floor(getServerRam("Server-0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24-25")[0] / 2.6)));
+
+}
+
+tprint("Spreaded and Executed on Purchased Servers!");
